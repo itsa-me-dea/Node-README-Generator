84fbfc6684abde55b31ed7564e0157ae83e9b932
itsa-me-dea <wachadea@gmail.com>
2023-05-29 20:36:00 -0700

    corrected README to include prompted info

---
+++ Develop/README.md
@@ -0,0 +1,34 @@
+# na
+
+### **[Description](#description) | [Installation](#installation) | [Usage](#usage) | [Credits](#credits) | [License](#license)**
+
+## Description
+
+na.
+
+na.
+
+na.
+
+na.
+
+## Installation
+
+na.
+
+## Usage
+
+na.
+
+![alt text](na)
+
+## Credits
+
+- na.
+- na.
+- na.
+
+
+## License
+
+Please refer to the [None](/LICENSE) within the repository.
--- Develop/index.js
+++ Develop/index.js
@@ -82,52 +82,45 @@ const questions = () => {
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
-const generateMD = ({ name, location, github, linkedin }) =>
-`# ${data.title}
+const generateMD = ({ project, username, email, what, why, how, urXP, installation, usage, screenshot, collaborators, thirdParty, tutorials, license}) =>
+`# ${project}
 
-### **[Description](#description) | [Installation](#installation) | [Usage](#usage) | [Credits](#credits) | [License](#license)**
+### **[Description](#description) | [Installation](#installation) | [Usage](#usage) | [Credits](#credits) | [License](#license) | [Questions](#questions)**
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+${what}.
 
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+${why}.
 
-## Table of Contents (Optional)
+${how}.
 
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
+${urXP}.
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+${installation}.
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
+${usage}.
 
-To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
-
-![alt text](assets/images/screenshot.png)
+![alt text](${screenshot})
 
 ## Credits
 
-List your collaborators, if any, with links to their GitHub profiles.
+- ${collaborators}.
+- ${thirdParty}.
+- ${tutorials}.
 
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
 
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).`;
+Please refer to the [${license}](/LICENSE) within the repository.
+
+## Questions
+
+My gitHub username is [${username}](https://github.com/${username}), and you can contact me at ${email}.`;
 
 // TODO: Create a function to initialize app
 const init = () => {
